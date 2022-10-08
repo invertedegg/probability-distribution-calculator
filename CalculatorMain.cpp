214d5a7a798fd4ff4411a58a631b078f4a7c3a54
invertedegg <invertedegg435@gmail.com>
2022-10-08 15:42:24 -0400

    Added Negative Binomial calculation to main

--- CalculatorMain.cpp
+++ CalculatorMain.cpp
@@ -1,8 +1,10 @@
 #include "Binomial.h"
+#include "NBinomial.h"
 #include <stdio.h>
 #include <iostream>
 using namespace std;
 using namespace B;
+using namespace NB;
 
 void header()
 {
@@ -18,7 +20,7 @@ int chooseDistribution()
 
     while(c > 2 || c < 0)
     {
-        cout << "Please choose a distribution to calculate.\nBinomial: 1\nNegative Binomial: 2\nExit: 0" << endl;
+        cout << "Please choose a distribution to calculate.\nBinomial: 1\nNegative Binomial: 2\nExit: 0\n\n:";
         cin >> c;
     }
     printf("--------------------------------------------\n");
@@ -30,7 +32,7 @@ int chooseAnswer1()
     int c = 50;
     while (c > 2 || c < 0)
     {
-        cout << "Please choose which condition you are solving for.\nExact Probability: 1\nCumulative Probability: 2" << endl;
+        cout << "Please choose which condition you are solving for.\nExact Probability: 1\nCumulative Probability: 2\n\n:";
         cin >> c;
     }
     printf("--------------------------------------------\n");
@@ -51,23 +53,27 @@ int main()
         switch(choice)
         {
             case 1:
-            Binomial bd;
+                // Binomial Distribution
+                Binomial bd;
             
-            choice = chooseAnswer1();
-            switch (choice)
-            {
-            case 1:
-                ans = bd.getDataExact();
-                break;
-            case 2:
-                ans = bd.getDataCumulative();
-                default:
-                    break;
+                choice = chooseAnswer1();
+                switch (choice){
+                    case 1:
+                        ans = bd.getDataExact();
+                        break;
+                    case 2:
+                        ans = bd.getDataCumulative();
+                        break;
+                    default:
+                        break;
                 }
                 break;
 
             case 2:
-                printf("you chose negative binomial (Not implemented yet)");
+                // Negative Binomial Distribution
+                NBinomial nbd;
+                                
+                ans = nbd.getDataExact();
                 break;
         }
 
